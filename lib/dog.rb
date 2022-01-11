9bd1e1a1b70684fe0552e22d64fc47d1a871289e
Chris Wiker <cwiker7@gmail.com>
2022-01-11 11:03:58 -0500

    commit tree 16442e5b4e3c098780198e48957f1fe4a8aaea22

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+    def sit 
+        puts "The Dog is sitting"
+    end 
+ 
+
+
+    def bark
+        puts "Woof!"
+    end
+end
+
+fido = Dog.new
+fido.bark 
+
+snoop = Dog.new
+snoop.bark
